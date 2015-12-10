e09c666daa38b035f99e3e1d4f4d507e25aad019
Beau Bechdol <bbechdol@gmail.com>
2015-12-10 14:42:29 -0700

    deleted comments

--- scripts/kirby_dance.js
+++ scripts/kirby_dance.js
@@ -1,8 +1,3 @@
-/** 
- * Make Kirby dance in your browser's JavaScript console. 
- * 
- * Great to run in your coworker's console when they forget to lock their computer.
- */
 var i = 0;
 var KFRAMES = [ "<('.'<)", "^('.')^", "(>'.')>", "^('.')^" ];
 setInterval( function () {
